fb7c1f878969eb1de0c50bb2de24ff2004c5e829
Artem Sokolov <agsokolov@diasoft.ru>
2021-04-15 19:26:42 +0300

    add @RunWith

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -2,7 +2,10 @@ import junit.framework.TestCase;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
 
+@RunWith(BlockJUnit4ClassRunner.class)
 public class MyTest extends TestCase {
 //public class MyTest {
 
